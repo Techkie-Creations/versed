a652289482e4025aebd3ca538803dd75f0bab834
Techkie Creations <technoworldcreations@gmail.com>
2025-05-06 23:12:45 +0200

    anonymous navbar completed

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
 
 <template>
   <header class="w-full px-8 py-4 flex justify-between items-center border-b">
@@ -6,14 +8,32 @@
     <nav class="w-1/3">
       <ul class="flex justify-between items-center">
         <li>
-          <a
-            href="#"
+          <RouterLink
+            to="#"
             class="p-2 border-2 rounded bg-alice text-eerie hover:border-2 hover:border-alice hover:bg-eerie hover:text-alice"
-            >Home</a
+            >Home</RouterLink
           >
         </li>
-        <li><a href="#" class="">About Us</a></li>
-        <li><a href="#" class="">Contact Us</a></li>
+        <li>
+          <RouterLink
+            to="#"
+            class="p-2 border-2 rounded bg-alice text-eerie hover:border-2 hover:border-alice hover:bg-eerie hover:text-alice"
+            >About</RouterLink
+          >
+        </li>
+        <li>
+          <RouterLink
+            to="#"
+            class="p-2 border-2 rounded bg-alice text-eerie hover:border-2 hover:border-alice hover:bg-eerie hover:text-alice"
+            >Contact Us</RouterLink
+          >
+        </li>
+        <RouterLink to="/"
+          ><i
+            class="pi pi-user border-2 p-2 rounded text-2xl hover:cursor-pointer hover:bg-alice hover:text-eerie"
+          >
+          </i
+        ></RouterLink>
       </ul>
     </nav>
   </header>
